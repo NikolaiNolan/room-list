1beb330abeff05fe344a6686b4300e95288f7bd4
Nikolai Nolan <atlassian@nikol.ai>
2019-05-24 23:13:22 -0500

    Add e-mail notifications

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
 
+const mailNotification = require('./mailNotification');
 const updateVariables = require('./updateVariables');
 
 module.exports = {
   updateVariables: functions.https.onRequest(updateVariables),
   scheduledUpdateVariables: functions.pubsub.schedule('every day 04:00').timeZone('America/Chicago').onRun(updateVariables),
+  mailNotificationOnAdd: functions.database.ref('cons/{conId}/people/{roomId}/{personId}').onCreate(snapshot => mailNotification(snapshot, 'add')),
+  mailNotificationOnRemove: functions.database.ref('cons/{conId}/people/{roomId}/{personId}').onDelete(snapshot => mailNotification(snapshot, 'remove')),
 };
---
+++ functions/mailNotification.js
@@ -0,0 +1,56 @@
+const format = require('date-fns/format');
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
+
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: functions.config().gmail.email,
+    pass: functions.config().gmail.password,
+  },
+});
+
+module.exports = async (personSnapshot, action) => {
+  const person = personSnapshot.val();
+
+  const conSnapshot = await personSnapshot.ref.parent.parent.parent.once('value');
+  const con = conSnapshot.val();
+
+  const userSnapshot = await admin.database().ref(`users/${personSnapshot.key}`).once('value');
+  const user = userSnapshot.val();
+
+  const actionClause = action === 'add' ? 'added to' : 'removed from';
+  let roomClause = '';
+  let rideClause = '';
+
+  if (action === 'add') {
+    const arrivalDay = format(person.dates.arrival, 'EEEE');
+    const departureDay = format(person.dates.departure, 'EEEE');
+    roomClause = `, for ${arrivalDay} through ${departureDay}`;
+    if (person.ride) {
+      let rideDirection;
+      if (person.ride.to) {
+        rideDirection = person.ride.from ? 'to and from' : 'to';
+      } else {
+        rideDirection = 'from';
+      }
+      rideClause = `, riding ${rideDirection} the convention`;
+    }
+  }
+
+  const mailOptions = {
+    from: '"Con Room List" <rooms@nikol.ai>',
+    to: `"Nikolai Nolan" <${functions.config().gmail.email}>`,
+    subject: `${con.name} room update`,
+    text: `${user.name} was ${actionClause} the ${con.name} room${roomClause}${rideClause}.`,
+  };
+
+  try {
+    await mailTransport.sendMail(mailOptions);
+  } catch(error) {
+    console.error(error);
+  }
+
+  return null;
+};
