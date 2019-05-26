0f5c3fd20f7be627148760fd1ab90421b4b003cf
Nikolai Nolan <atlassian@nikol.ai>
2019-05-26 15:32:13 -0500

    Schedule function in UTC

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const updateVariables = require('./updateVariables');
 
 module.exports = {
   updateVariables: functions.https.onRequest(updateVariables),
-  scheduledUpdateVariables: functions.pubsub.schedule('every day 04:00').timeZone('America/Chicago').onRun(updateVariables),
+  scheduledUpdateVariables: functions.pubsub.schedule('every day 09:00').onRun(updateVariables),
   mailNotificationOnAdd: functions.database.ref('cons/{conId}/people/{roomId}/{personId}').onCreate(snapshot => mailNotification(snapshot, 'add')),
   mailNotificationOnRemove: functions.database.ref('cons/{conId}/people/{roomId}/{personId}').onDelete(snapshot => mailNotification(snapshot, 'remove')),
 };
