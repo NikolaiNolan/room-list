e86a0bcd1c00cc0862cbbee7d190f94d831a141e
Nikolai Nolan <atlassian@nikol.ai>
2019-05-25 20:06:26 -0500

    Add icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,6 +57,80 @@ export default {
 
   modules: [
     [
+      'nuxt-rfg-icon',
+      {
+        masterPicture: 'static/icon.svg',
+        force: true,
+        rfg: {
+          "masterPicture": "TODO: Path to your master picture",
+          "iconsPath": "/",
+          "design": {
+            "ios": {
+              "masterPicture": {
+                "type": "inline",
+                "content": "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNS4xLjAsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iODgzLjM0cHgiIGhlaWdodD0iODgzLjM0cHgiIHZpZXdCb3g9Ii0zOTUuNDggLTM0NS42NyA4ODMuMzQgODgzLjM0Ig0KCSBlbmFibGUtYmFja2dyb3VuZD0ibmV3IC0zOTUuNDggLTM0NS42NyA4ODMuMzQgODgzLjM0IiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCgkgPHJlY3QgeD0iLTUwJSIgeT0iLTUwJSIgaGVpZ2h0PSIxNTAlIiB3aWR0aD0iMTUwJSIgZmlsbD0iI2RkNzMxZCIgLz4NCjxwb2x5Z29uIGZpbGw9IiNERDczMUQiIHBvaW50cz0iLTIwOC44MTIsLTIxLjc3OCA0Ni4xOSwtMjU3LjMzNSAxNDguMTg2LC0xOTguNDQ2IDI1MC4xOTQsMjcyLjY2NyAtNTUuODA2LDM5MC40NDYNCgktMTU3LjgxNCwzMzEuNTU3ICIvPg0KPHBvbHlnb24gZmlsbD0iI0ZEODcxMyIgcG9pbnRzPSIxNDguMTg2LC0xOTguNDQ2IDI1MC4xOTQsLTEzOS41NTcgMzAxLjE5MiwyNDMuMjIzIDQ2LjE5LDQ0OS4zMzUgLTU1LjgwNiwzOTAuNDQ2IC01NS44MDYsMTU0Ljg4OQ0KCTQ2LjE5LDIxMy43NzggMTk5LjE5NiwzMzEuNTU3IDE0OC4xODYsMTU0Ljg4OSAtNTUuODA2LDM3LjExMSAtMTA2LjgxNiwtMTM5LjU1NyA0Ni4xOSwtMjEuNzc4IDE0OC4xODYsMzcuMTExICIvPg0KPHBvbHlnb24gZmlsbD0iI0ZGRkZGRiIgcG9pbnRzPSItMjU5LjgxLC04MC42NjcgNDYuMTksLTI1Ny4zMzUgNDYuMTksLTIxLjc3OCAtNTUuODA2LC04MC42NjcgLTU1LjgwNiwzNy4xMTEgLTE1Ny44MTQsLTIxLjc3OA0KCS0xNTcuODE0LDMzMS41NTcgLTI1OS44MSwyNzIuNjY3ICIvPg0KPHBvbHlnb24gZmlsbD0iI0ZGRkZGRiIgcG9pbnRzPSIzNTIuMTksMjcyLjY2NyA0Ni4xOSw0NDkuMzM1IDQ2LjE5LDIxMy43NzggMTQ4LjE4NiwyNzIuNjY3IDE0OC4xODYsMTU0Ljg4OSAyNTAuMTk0LDIxMy43NzgNCgkyNTAuMTk0LC0xMzkuNTU3IDM1Mi4xOSwtODAuNjY3ICIvPg0KPC9zdmc+DQo="
+              },
+              "pictureAspect": "noChange",
+              "assets": {
+                "ios6AndPriorIcons": false,
+                "ios7AndLaterIcons": false,
+                "precomposedIcons": false,
+                "declareOnlyDefaultIcon": true
+              }
+            },
+            "desktopBrowser": {},
+            "windows": {
+              "pictureAspect": "noChange",
+              "backgroundColor": "#de7317",
+              "onConflict": "override",
+              "assets": {
+                "windows80Ie10Tile": false,
+                "windows10Ie11EdgeTiles": {
+                  "small": false,
+                  "medium": true,
+                  "big": false,
+                  "rectangle": false
+                }
+              }
+            },
+            "androidChrome": {
+              "masterPicture": {
+                "type": "inline",
+                "content": "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4NCjwhLS0gR2VuZXJhdG9yOiBBZG9iZSBJbGx1c3RyYXRvciAxNS4xLjAsIFNWRyBFeHBvcnQgUGx1Zy1JbiAuIFNWRyBWZXJzaW9uOiA2LjAwIEJ1aWxkIDApICAtLT4NCjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DQo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4Ig0KCSB3aWR0aD0iODgzLjM0cHgiIGhlaWdodD0iODgzLjM0cHgiIHZpZXdCb3g9Ii0zOTUuNDggLTM0NS42NyA4ODMuMzQgODgzLjM0Ig0KCSBlbmFibGUtYmFja2dyb3VuZD0ibmV3IC0zOTUuNDggLTM0NS42NyA4ODMuMzQgODgzLjM0IiB4bWw6c3BhY2U9InByZXNlcnZlIj4NCgkgPHJlY3QgeD0iLTUwJSIgeT0iLTUwJSIgaGVpZ2h0PSIxNTAlIiB3aWR0aD0iMTUwJSIgZmlsbD0iI2RkNzMxZCIgLz4NCjxwb2x5Z29uIGZpbGw9IiNERDczMUQiIHBvaW50cz0iLTIwOC44MTIsLTIxLjc3OCA0Ni4xOSwtMjU3LjMzNSAxNDguMTg2LC0xOTguNDQ2IDI1MC4xOTQsMjcyLjY2NyAtNTUuODA2LDM5MC40NDYNCgktMTU3LjgxNCwzMzEuNTU3ICIvPg0KPHBvbHlnb24gZmlsbD0iI0ZEODcxMyIgcG9pbnRzPSIxNDguMTg2LC0xOTguNDQ2IDI1MC4xOTQsLTEzOS41NTcgMzAxLjE5MiwyNDMuMjIzIDQ2LjE5LDQ0OS4zMzUgLTU1LjgwNiwzOTAuNDQ2IC01NS44MDYsMTU0Ljg4OQ0KCTQ2LjE5LDIxMy43NzggMTk5LjE5NiwzMzEuNTU3IDE0OC4xODYsMTU0Ljg4OSAtNTUuODA2LDM3LjExMSAtMTA2LjgxNiwtMTM5LjU1NyA0Ni4xOSwtMjEuNzc4IDE0OC4xODYsMzcuMTExICIvPg0KPHBvbHlnb24gZmlsbD0iI0ZGRkZGRiIgcG9pbnRzPSItMjU5LjgxLC04MC42NjcgNDYuMTksLTI1Ny4zMzUgNDYuMTksLTIxLjc3OCAtNTUuODA2LC04MC42NjcgLTU1LjgwNiwzNy4xMTEgLTE1Ny44MTQsLTIxLjc3OA0KCS0xNTcuODE0LDMzMS41NTcgLTI1OS44MSwyNzIuNjY3ICIvPg0KPHBvbHlnb24gZmlsbD0iI0ZGRkZGRiIgcG9pbnRzPSIzNTIuMTksMjcyLjY2NyA0Ni4xOSw0NDkuMzM1IDQ2LjE5LDIxMy43NzggMTQ4LjE4NiwyNzIuNjY3IDE0OC4xODYsMTU0Ljg4OSAyNTAuMTk0LDIxMy43NzgNCgkyNTAuMTk0LC0xMzkuNTU3IDM1Mi4xOSwtODAuNjY3ICIvPg0KPC9zdmc+DQo="
+              },
+              "pictureAspect": "noChange",
+              "themeColor": "#ffffff",
+              "manifest": {
+                "display": "standalone",
+                "orientation": "notSet",
+                "onConflict": "override",
+                "declared": true
+              },
+              "assets": {
+                "legacyIcon": false,
+                "lowResolutionIcons": false
+              }
+            },
+            "safariPinnedTab": {
+              "masterPicture": {
+                "type": "inline",
+                "content": "CTxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2aWV3Qm94PSIxNC4xOSA2NCA2NCA2NCI+DQoJPHBvbHlnb24gZmlsbD0iI0ZGRkZGRiIgcG9pbnRzPSIxOC40NzcsODAgNDYuMTksNjQgNDYuMTksODUuMzMzIDM2Ljk1Myw4MCAzNi45NTMsOTAuNjY3IDI3LjcxNCw4NS4zMzMgMjcuNzE0LDExNy4zMzMgMTguNDc3LDExMiIvPg0KCTxwb2x5Z29uIGZpbGw9IiNGRkZGRkYiIHBvaW50cz0iNzMuOTAzLDExMiA0Ni4xOSwxMjggNDYuMTksMTA2LjY2NyA1NS40MjgsMTEyIDU1LjQyOCwxMDEuMzMzIDY0LjY2NiwxMDYuNjY3IDY0LjY2Niw3NC42NjcgNzMuOTAzLDgwIi8+DQo8L3N2Zz4NCg=="
+              },
+              "pictureAspect": "silhouette",
+              "themeColor": "#5bbad5"
+            }
+          },
+          "settings": {
+            "scalingAlgorithm": "Mitchell",
+            "errorOnImageTooSmall": false,
+            "readmeFile": false,
+            "htmlCodeFile": false,
+            "usePathAsIs": false
+          }
+        },
+      },
+    ],
+    [
       '@nuxtjs/pwa',
       { icon: false },
     ],
